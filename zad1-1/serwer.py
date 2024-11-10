359bd3a3bd8da86d9ae8c479ba3902d8c5b7eeb0
bogumil.stoma <bogumil.stoma@gmail.com>
2024-11-10 15:01:46 +0100

    Dodano klienta i serwer w pythonie

---
+++ zad1-1/serwer.py
@@ -0,0 +1,27 @@
+import socket
+
+PORT = 12345
+HOST = '0.0.0.0'
+MAX_BUF_SIZE = 160000
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    
+    print(f"Server listening on port {port}...")
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind((HOST, PORT))
+        
+        while True:
+            data, address = s.recvfrom(MAX_BUF_SIZE)
+            length = (data[0] << 8) | data[1]
+            if length != len(data) - 2:
+                print("Received datagram length mismatch")
+            else:
+                print(f"Received datagram of length {len(data)}")
+
+            response = b"OK"
+            s.sendto(response, address)
+
+if __name__ == "__main__":
+    main()
