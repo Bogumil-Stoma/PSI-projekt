e967c3e2f8cbce3839739f5b44ec5f479c97581b
Sebastian <sebastian.abramowski@onet.pl>
2025-01-03 01:16:28 +0100

    Client done

--- projekt/client.py
+++ projekt/client.py
@@ -1,15 +1,69 @@
+from typing import Optional, Callable
 import socket
 import struct
 import os
 import sys
 import utils
+import threading
+import time
 
 
-# TODO:W oddzielnym wątku nasluchujemy na wiadmości od servera:
-# - "OK" -> "Message authenticity verification failed."
-# - "FAIL" -> "Message authenticity verified successfully."
-# - "DISCONNECT" -> "Server disconnected. Closing connection."
-# - w tym wątku wykorzystać instację ThreadPrinter do wypisywania komunikatów
+class ServerListener(threading.Thread):
+    def __init__(self, client_socket: socket.socket, printer: utils.ThreadPrinter,
+                 symmetric_key, close_connection_callback: Callable[[], None],
+                 notify_and_disconnect_callback: Callable[[], None]):
+        super().__init__()
+        self.client_socket = client_socket
+        self.printer = printer
+        self.stop_event = threading.Event()
+        self.symmetric_key = symmetric_key
+        self.close_connection_callback = close_connection_callback
+        self.notify_and_disconnect_callback = notify_and_disconnect_callback
+
+    def print(self, *args, **kwargs):
+        self.printer.print(*args, **kwargs)
+
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        """Processes server messages. Activated after completing key exchange."""
+        try:
+            while not self.stop_event.is_set():
+                message_size_data = utils.receive_data(self.client_socket, 4)
+                message_size = struct.unpack("!I", message_size_data)[0]
+                iv = utils.receive_data(self.client_socket, utils.AES_BLOCK_SIZE)  # 16B
+                ciphertext = utils.receive_data(self.client_socket, message_size)
+                mac = utils.receive_data(self.client_socket, 32)  # 32B
+
+                calculated_mac = utils.calculate_hmac(ciphertext, self.symmetric_key)
+                if mac != calculated_mac:
+                    self.print("Authentication failed - something is wrong with the server")
+                    self.print("MAC received: ", mac, "\nMAC calculated: ", calculated_mac)
+                    self.notify_and_disconnect_callback()
+
+                    return
+
+                decrypted_message = utils.aes_cbc_decrypt(iv, ciphertext, self.symmetric_key)
+                self.print(f"Received text: {decrypted_message}")
+
+                if decrypted_message == utils.ServerMessages.OK:
+                    self.print("[From Server] Message authenticity verified successfully.")
+                elif decrypted_message == utils.ServerMessages.FAIL:
+                    self.notify_and_disconnect_callback()
+                    self.print("[From Server] Message authenticity verification failed.")
+                    self.stop_event.set()
+                elif decrypted_message == utils.ServerMessages.END_SESSION:
+                    self.close_connection_callback()
+                    self.print("[From Server] Session ended by server. Disconnecting...")
+                    self.stop_event.set()
+                else:
+                    self.print(f"[From Server] Unknown message: {decrypted_message}")
+        except Exception as e:
+            self.print(f"Caught Error in ServerListener: {e}")
+        finally:
+            self.print("ServerListener stopped.")
+
 
 class DiffieHellmanClient:
     def __init__(self, host, port, verbose, g, p):
@@ -22,6 +76,16 @@ class DiffieHellmanClient:
         self.verbose = verbose
         self.printer = utils.ThreadPrinter()
         self.printer.start()
+        self.server_listener: Optional[ServerListener] = None
+
+    def stop(self):
+        self.notify_and_dissconnect()
+        if self.server_listener:
+            self.server_listener.stop()
+            self.server_listener.join()
+        if self.printer:
+            self.printer.stop()
+            self.printer.join()
 
     def print(self, *args, **kwargs):
         self.printer.print(*args, **kwargs)
@@ -48,7 +112,6 @@ class DiffieHellmanClient:
         self.client_socket.sendall(hello_message)
         self.print_if_verbose(f"[V] Sent ClientHello with A={public_key}, p={self.p}, g={self.g}")
 
-        # TODO: trzeba odczytać od serwera ServerHello w oddzielnym wątku
         server_hello = utils.receive_data(self.client_socket, 15)  # 11 + 4
         server_hello_msg, server_public_key = struct.unpack("!11sI", server_hello)
 
@@ -66,23 +129,30 @@ class DiffieHellmanClient:
     def connect(self):
         try:
             self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_socket.settimeout(10)
+            self.client_socket.settimeout(None) # continuous listenening for messages
             self.print(f"Connecting to server at {self.host}:{self.port}...")
             self.client_socket.connect((self.host, self.port))
 
-            # TODO: już zestowione połączenie wykorzystać w oddzilym wątku do nasluchiwania na wiadmości od servera, podać isntację threadPrintera
-            self.print("Connected")
             self.symmetric_key = self.perform_key_exchange(self.private_key, self.public_key)
+            self.print("Successfully connected to the server")
             self.connected = True
+
+            self.server_listener = ServerListener(self.client_socket, self.printer,
+                                                  self.symmetric_key, self.close_connection,
+                                                  self.notify_and_dissconnect)
+            self.server_listener.start()
         except Exception as e:
-            self.print(f"Connection error: {e}")
+            self.print(f"Caught Connection Error: {e}")
 
     def close_connection(self):
-        self.client_socket.close()
-        self.connected = False
+        if self.connected:
+            self.client_socket.close()
+            self.connected = False
 
     def notify_and_dissconnect(self):
-        # TODO: wysyłamy do serwera wiadomość "EndSession" żeby sobie zamknął gniazdo
+        if self.connected:
+            self.send_message(utils.ServerMessages.END_SESSION) # notify server
+            time.sleep(0.1) # give server time to read the message
 
         self.close_connection()
         self.print("Notififed server and disconnected")
@@ -105,17 +175,11 @@ class DiffieHellmanClient:
 
             self.client_socket.sendall(final_message)
 
-            # TODO: czekamy na wiadomość od server OK / FAIL
-            # najpierw czytamy 4B a potem resztę
-            # OK -> "Message authenticity verification failed."
-            # FAIL -> "Message authenticity verified successfully."
-            # Zrobić to trzeba w oddzielnym wątku
-
         except ConnectionError:
-            self.print("Lost connection to the server")
+            self.print("Caught Error: lost connection to the server")
             self.connected = False
         except Exception as e:
-            self.print(f"Error while sending the message to server: {e}")
+            self.print(f"Caught Error while sending the message to server: {e}")
             self.connected = False
             raise e
 
@@ -152,11 +216,9 @@ class DiffieHellmanClient:
                 if (not self.connected):
                     self.print("Server not connected!  Use 'connect' command")
                 else:
-                    # TODO: wyslić wiadmość do servera 'EndSession'
                     self.notify_and_dissconnect()
             elif command == "shutdown":
-                if self.connected:
-                    self.notify_and_dissconnect()
+                self.stop()
                 self.print("Shutting down client...")
                 sys.exit(0)
 
